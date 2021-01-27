48a02f580e18b0d40791ed1710181a3e1de244f6
JT <jtew396@gmail.com>
2021-01-26 21:39:32 -0600

    Finished Google Maps & Places tutorial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,49 +1,188 @@
 import React, { Component } from 'react';
+import apiConfig from './apiKeys';
 import {
     withGoogleMap,
     withScriptjs,
     GoogleMap,
+    useLoadScript,
     Marker,
     InfoWindow
-} from "react-google-maps";
+} from "@react-google-maps/api";
+import { formatRelative } from "date-fns";
 
-import './App.css';
-import WeekContainer from './components/weather/WeekContainer';
+import usePlacesAutocomplete, {
+    getGeocode,
+    getLatLng,
+} from "use-places-autocomplete";
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from "@reach/combobox";
+import "@reach/combobox/styles.css";
 
-import LoginForm from './components/pictures/LoginForm';
-import RegisterForm from './components/pictures/RegisterForm';
+const libraries = ["places"];
+const mapContainerStyle = {
+    width: '100vw',
+    height: '100vh',
+}
+const center = {
+    lat: 35.13727,
+    lng: -89.96263,
+}
+const options = {
+    disableDefaultUI: true,
+    zoomControl: true
+}
 
-function Map() {
+export default function App() {
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: apiConfig.googleKey,
+        libraries,
+    });
+    const [markers, setMarkesrs] = React.useState([]);
+    const [selected, setSelected] = React.useState(null);
+
+    const onMapClick = React.useCallback((event) => {
+        setMarkesrs(current => [
+            ...current, 
+            {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+                time: new Date(),
+            },
+        ]);
+    }, []);
+
+    const mapRef = React.useRef();
+    const onMapLoad = React.useCallback((map) => {
+        mapRef.current = map;
+    }, []);
+
+    const panTo = React.useCallback(({lat, lng}) => {
+        mapRef.current.panTo({lat, lng});
+        mapRef.current.setZoom(14);
+    }, []);
+    
+    if (loadError) return "Error loading maps";
+    if(!isLoaded) return "Loading Maps";
+
     return (
-        <GoogleMap 
-            defaultZoom={10} 
-            defaultCenter={{ lat: 35.1495, lng: -90.0490 }}
-        />
+        <div className="App">
+            <h1>
+                Skate Spots <span role="img" aria-label="skateboard">🛹</span>
+            </h1>
+
+            <Search panTo={panTo} />
+            <Locate panTo={panTo} />
+
+            <GoogleMap 
+                mapContainerStyle={mapContainerStyle} 
+                zoom={12} 
+                center={center}
+                options={options}
+                onClick={onMapClick}
+                onLoad={onMapLoad}
+            >
+                {markers.map((marker) => (
+                    <Marker 
+                        key={marker.time.toISOString()} 
+                        position={{lat: marker.lat, lng: marker.lng}}
+                        onClick={() => {
+                            setSelected(marker);
+                        }}
+                    />
+                ))}
+
+
+                {selected ? (
+                    <InfoWindow 
+                        position={{ lat: selected.lat, lng: selected.lng }} 
+                        onCloseClick={() => {
+                            setSelected(null);
+                        }}
+                    >
+                        <div>
+                            <h5>Skate spot!</h5>
+                            <p>Discovered {formatRelative(selected.time, new Date())}</p>
+                        </div>
+                    </InfoWindow>
+                ) : null}
+            </GoogleMap>
+        </div>
     );
 }
 
-const WrappedMap = withScriptjs(withGoogleMap(Map));
+function Locate({panTo}) {
+    return (
+        <button 
+            className="locate" 
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                    (position) => {
+                        panTo({
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        });
+                    }, 
+                    () => null, 
+                );
+            }}
+        >
+            <img src="compass.svg" alt="compass - locate me" />
+        </button>
+    );
+}
 
-export default class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+function Search({ panTo }) {
+    const {
+        ready, 
+        value, 
+        suggestions: { status, data }, 
+        setValue, 
+        clearSuggestions,
+    } = usePlacesAutocomplete({
+        requestOptions: {
+            location: { lat: () => 35.13727, lng: () => -89.96263 },
+            radius: 200000
+        },
+    });
 
-        };
-    }
+    return (
+        <div className="search">
+            <Combobox 
+                onSelect={async (address) => {
+                    setValue(address, false);
+                    clearSuggestions();
 
-
-
-    render() {
-        return (
-            <div className="App" style={{ width: '100vw', height: '100vh' }}>
-                <WrappedMap 
-                    googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places`} 
-                    loadingElement={<div style={{ height: "100%" }} />}
-                    containerElement={<div style={{ height: "100%" }} />}
-                    mapElement={<div style={{ height: "100%" }} />} 
+                    try {
+                        const results = await getGeocode({ address });
+                        const { lat , lng } = await getLatLng(results[0]);
+                        panTo({ lat, lng });
+                    } catch(error) {
+                        console.log("error!");
+                    }
+                }}
+            >
+                <ComboboxInput 
+                    value={value} 
+                    onChange={(e) => {
+                        setValue(e.target.value);
+                    }}
+                    disabled={!ready}
+                    placeholder="Enter an address"
                 />
-            </div>
-        );
-    }
+                <ComboboxPopover>
+                    <ComboboxList>
+                        {status === "OK" && 
+                            data.map(({ id, description }) => (
+                                <ComboboxOption key={id} value={description} />
+                            ))}
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>
+    );
 }
